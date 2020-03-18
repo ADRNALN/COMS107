a079dd522e7b21d4ebeaf8cca54255ecef1679c1
Timon Nadarevic <32118783+Tiimon@users.noreply.github.com>
2020-03-18 14:44:39 -0500

    Add files via upload

---
+++ Assignment7/SRMS/SRMS/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SRMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPhysics = new System.Windows.Forms.TextBox();
+            this.txtChemistry = new System.Windows.Forms.TextBox();
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.retrieveTextBox = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(13, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(13, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Physics";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(12, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Chemistry";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(13, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Mathematics";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(141, 91);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(141, 122);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 7;
+            // 
+            // txtPhysics
+            // 
+            this.txtPhysics.Location = new System.Drawing.Point(141, 153);
+            this.txtPhysics.Name = "txtPhysics";
+            this.txtPhysics.Size = new System.Drawing.Size(100, 20);
+            this.txtPhysics.TabIndex = 8;
+            // 
+            // txtChemistry
+            // 
+            this.txtChemistry.Location = new System.Drawing.Point(141, 185);
+            this.txtChemistry.Name = "txtChemistry";
+            this.txtChemistry.Size = new System.Drawing.Size(100, 20);
+            this.txtChemistry.TabIndex = 9;
+            // 
+            // txtMath
+            // 
+            this.txtMath.Location = new System.Drawing.Point(141, 217);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(100, 20);
+            this.txtMath.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(324, 91);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(109, 49);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Location = new System.Drawing.Point(324, 304);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(109, 49);
+            this.btnRetrieve.TabIndex = 12;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label7.Location = new System.Drawing.Point(12, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Retrieve Student";
+            // 
+            // retrieveTextBox
+            // 
+            this.retrieveTextBox.Location = new System.Drawing.Point(16, 304);
+            this.retrieveTextBox.Name = "retrieveTextBox";
+            this.retrieveTextBox.Size = new System.Drawing.Size(289, 102);
+            this.retrieveTextBox.TabIndex = 14;
+            this.retrieveTextBox.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(302, 428);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Created By: Timon Nadarevic";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(324, 357);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 49);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.retrieveTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnRetrieve);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtMath);
+            this.Controls.Add(this.txtChemistry);
+            this.Controls.Add(this.txtPhysics);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student Record Management System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPhysics;
+        private System.Windows.Forms.TextBox txtChemistry;
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox retrieveTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Assignment7/SRMS/SRMS/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SRMS
+{
+    public partial class Form1 : Form
+    {
+        //intialize 2d array
+        public string[,] studentId = new string[100,2];
+        public int[,] studentClasses = new int[100, 4];
+
+        public Form1()
+        {
+            InitializeComponent();
+            retrieveTextBox.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            int id;
+            int physicsScore;
+            int chemScore;
+            int mathScore;
+            try
+            {
+                id = int.Parse(txtID.Text);
+                physicsScore = int.Parse(txtPhysics.Text);
+                chemScore = int.Parse(txtChemistry.Text);
+                mathScore = int.Parse(txtMath.Text);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            //store id and name to first array
+            studentId[id, 0] = id.ToString();
+            studentId[id, 1] = name;
+
+            //store other stuff into second array
+            studentClasses[id, 0] = id;
+            studentClasses[id, 1] = chemScore;
+            studentClasses[id, 2] = mathScore;
+            studentClasses[id, 3] = physicsScore;
+
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            string id = Interaction.InputBox("Please enter StudentId and click OK to retrieve", "Retrieve Student Record");
+
+            int retrieveId = int.Parse(id);
+            string retrieveString = "";
+
+            retrieveString += "Student ID: " +studentId[retrieveId, 0] + "\n";
+            retrieveString += "Name: " + studentId[retrieveId, 1]  + "\n";
+
+           
+            retrieveString+= "Chemistry: " + studentClasses[retrieveId, 1] + "\n";
+            retrieveString += "Mathmatics: "+studentClasses[retrieveId, 2] + "\n";
+            retrieveString += "Physics: " +studentClasses[retrieveId, 3] + "\n";
+            
+            retrieveTextBox.Text = retrieveString;
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+           string id = Interaction.InputBox("Please enter StudentId and click OK to delete", "Delete Student Record");
+
+            int deleteId = int.Parse(id);
+
+            //delete id and name to first array
+            studentId[deleteId, 0] = "";
+            studentId[deleteId, 1] = "";
+
+            //delete other stuff into second array
+            studentClasses[deleteId, 0] = 0;
+            studentClasses[deleteId, 1] = 0;
+            studentClasses[deleteId, 2] = 0;
+            studentClasses[deleteId, 3] = 0;
+
+            MessageBox.Show("Student record Deleted!", "Delete");
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+             
+       }
+    }
+}
